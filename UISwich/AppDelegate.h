f5656e60f0ad42f81b3d0c48e272512a7a38d3fa
Apple <apple@smtp63.newsletterservice1.net>
2018-08-27 15:08:15 +0800

    Initial Commit

---
+++ UISwich/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISwich
+//
+//  Created by 橘子 on 2018/8/27.
+//  Copyright © 2018年 橘子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISwich/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISwich
+//
+//  Created by 橘子 on 2018/8/27.
+//  Copyright © 2018年 橘子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
